5cf2af5091fdefd8da7af53b900e5aad2cdcdb6e
bentengxiangdahai <69134539@qq.com>
2018-12-12 17:39:35 +0800

    接口測試及代碼

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/demo")
+public class DemoController {
+
+	@PostMapping("/queryDemo")
+	public Map<String, Object> queryDemo() throws Exception {
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("stat", 0);
+		resultMap.put("msg", "success");
+		resultMap.put("val", "demo-test");
+		return resultMap;
+	}
+}
