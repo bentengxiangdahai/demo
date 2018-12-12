7ef79a5dab48620f72ad8bd65c6b5ddbe1d1ae73
bentengxiangdahai <69134539@qq.com>
2018-12-12 17:02:46 +0800

    eclipse提交测试

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动测试
+ * @author 84832
+ *
+ */
 @SpringBootApplication
 public class DemoApplication {
 
