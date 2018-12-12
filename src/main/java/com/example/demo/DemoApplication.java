4ed69a4eb1dd30e40c87ad87e78935ef62483e1f
bentengxiangdahai <69134539@qq.com>
2018-12-12 18:04:38 +0800

    增加注释2018121201

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//增加注釋
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
